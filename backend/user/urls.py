b58b408e5cd24a4e0849b2f40285d4f15c08a5d1
Антон <antonbliznuk71@gmail.com>
2026-03-16 15:10:18 +0000

    connected my-info file in urls.py

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import UserRegistrationView, AdminRegistrationView
+from .views import UserRegistrationView, AdminRegistrationView, MyInfoView
 
 
 urlpatterns = [
     path("user/register/", UserRegistrationView.as_view(), name="register"),
     path("admin/register/", AdminRegistrationView.as_view(), name="admin-register"),
+    path("user/my-info/", MyInfoView.as_view(), name="my-info"),
 ]
